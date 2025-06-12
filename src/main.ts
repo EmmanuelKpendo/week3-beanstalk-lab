300f1b33fe0cfb9436b4a3f08fbae1323da64cd2
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2025-06-12 09:10:31 +0000

    refactor: update hbs implementation

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.setBaseViewsDir(join(__dirname, '..'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
   await app.listen(process.env.PORT ?? 3000);
